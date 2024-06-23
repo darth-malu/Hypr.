04d2c0e34df4c1cf36744cdc4e79944499770cca
darthmalu <justinmalu@gmail.com>
2024-06-23 09:25:20 +0300

    Major overhauls - Kitty, Hyprland,

--- .local/darthfiles/player_focus.sh
+++ .local/darthfiles/player_focus.sh
@@ -4,12 +4,19 @@ function active_player() {
     playerctl metadata --format '{{ playerName }}'
 }
 
+
+grap_spotify_pid () {
+    #extracts as pid: xxxx 
+    hyprctl clients | grep -i -A3 Spotify | grep pid | tr -d '[:blank:]' 
+}
+
 function focus_current_player () {
     local displaying_player=$(active_player)
+    spotify=$(grap_spotify_pid)
 
     case $displaying_player in
         *"spotify"*)
-            hyprctl dispatch focuswindow Spotify
+            hyprctl dispatch focuswindow ${spotify}
             ;;
         *"Lollypop"*)
             hyprctl dispatch focuswindow lollypop
