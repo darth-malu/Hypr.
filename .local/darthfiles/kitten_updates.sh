a3d83129d67baf5e0706ef14b90ad22f45110efa
darthmalu <justinmalu@gmail.com>
2024-07-10 09:03:53 +0300

    Cleaned up kitty_updates, unified css

--- .local/darthfiles/kitten_updates.sh
+++ .local/darthfiles/kitten_updates.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-fetch_updates () {
-    #checkupdates --nocolor | cut -d ' ' -f1 2>&1
-    checkupdates --nocolor|cut -d ' ' -f1
+dunst_after () {
+    local msgTag="pacman"
+    dunstify -t 3000 -a "pacman_update" -u critical -i "~/.local/darthfiles/iconss/pacman.png" -h string:x-dunst-stack-tag:$msgTag "Pacman updated packages"
 }
 
-kitten_inject () {
-    local pkgs=$(fetch_updates)
+dunst_aur () {
+    local msgTag="pacman"
+    dunstify -t 3000 -a "aur_update" -u critical -i "~/.local/darthfiles/iconss/pacman.png" -h string:x-dunst-stack-tag:$msgTag "AUR updated packages"
+    pkill -RTMIN+8 waybar
+}
 
+kill_kitty () {
+    kitty @ close-window --match env:KITTY_WINDOW_ID=$KITTY_WINDOW_ID
+}
+
+display_updates_only () 
+{
+    #nocolor very important
+    #prints out ansi color codes -yuck
+    updates="$(checkupdates --nocolor | cut -d ' ' -f1)"
+    for update in ${updates};do
+        printf "\t%s\n" "${update}"
+    done
+}
+
+display_updates_only 2> /dev/null
+updates="$(display_updates_only)"
+
+less_pkgs () 
+{
     local temp_file="$(mktemp /media/linux_hdd/update_history_script/temp_pac_qi_array/pac_updates.XXXXXX.txt)"
 
     # Check if the temporary file was created successfully
@@ -16,64 +38,78 @@ kitten_inject () {
         exit 1
     fi
 
-    for pkg in ${pkgs}; do
-        pacman -Qi "${pkg}" >> "$temp_file"
-    done
-    less -F "$temp_file" && rm $temp_file
+    # Iterate over each update, fetch its details and redirect output to $tempfile
+    while read -r update; do
+        pacman -Qi "$update" >> "$temp_file"
+    done <<< "$updates"
+    #less
+    less -F "$temp_file" && rm "$temp_file"
+
+    #alt way
+    #display_updates_only | while read -r update; do
+        #pacman -Qi "$update" >> "$temp_file"
+    #done
 }
 
-dunsst_after () {
-    local msgTag="pacman"
-    dunstify -t 3000 -a "pacman_update" -u critical -i "~/.local/darthfiles/iconss/pacman.png" \
-        -h string:x-dunst-stack-tag:$msgTag "Pacman updated packages"
-}
-
-dunsst_aur () {
-    local msgTag="pacman"
-    dunstify -t 3000 -a "aur_update" -u critical -i "~/.local/darthfiles/iconss/pacman.png" \
-        -h string:x-dunst-stack-tag:$msgTag "AUR updated packages"
-    pkill -RTMIN+8 waybar
-}
-
-kill_kitty () {
-    kitty @ close-window --match env:KITTY_WINDOW_ID=$KITTY_WINDOW_ID
-}
 
 display_or_not () {
-    local pkg=$(fetch_updates)
-    echo -e "The packages to be updated are listed below :\n"
-    echo -e "${pkg} \n" 
-    echo -e "would you like to see more verbose info on the packages? : \n \n* Yes\t\t - 1, \n* No\\t\t - None\n* HALT\t\t - 3\n* Pick Choice: "
-    read choice
-    echo "You picked: ${choice}"
+    #set -x
+    #is single line delimeted by spaces eg. pkg1 pkg2 pkg3
+    #local all_updates=$(display_updates_only)
 
-    case ${choice} in
-        "1")
-            kitten_inject \
-                && sudo pacman -Syu \
-                && pkill -RTMIN+8 waybar \
-                && dunsst_after
-            ;;
-        "3")
-            dunstify "QUITING"
-            kill_kitty
-            #exit 0
-            ;;
-        *)
-            sudo pacman -Syu \
-                && pkill -RTMIN+8 waybar \
-                && dunsst_after
-            ;;
-    esac
+    printf "\t\t\tPackages to be updated are:\n"
+    printf "\t\t\t...........................\n"
+    echo "${updates}"
+    echo
+
+    printf "\t\tChoose update style\n"
+    printf "\t\t...................\n"
+    printf "\tQuiet\t\t~>\t*Default\n\tverbose\t\t  \t 1\n\tHALT\t\t  \t 0\n\n"
+    #quiet = 1(default), minimal =2, halt =3, verbose = 1
+    read -p "       \`~> " choice
+
+    while true;do
+        case ${choice} in
+
+            #verbose
+            1)
+                printf "\npacman -Qi of packages (verbose)\n"
+                printf "\nLess is launching to display pacman -Qi pkgs\n"
+                sleep 1;less_pkgs ; break
+                ;;
+
+            #HALT
+            0)
+                dunstify "QUITING";break
+                #exit 0
+                ;;
+
+            #Default -> quiet
+            "")
+                break
+                ;;
+
+            *)
+                echo "Invalid Choice 1,2,3,blank";continue
+                ;;
+        esac
+    done
+    sudo pacman -Syu \
+        && pkill -RTMIN+8 waybar \
+        && dunst_after
 }
 
 aur_or_main () {
     case $1 in
         "aur")
-            paru -Sua && dunsst_aur
+            paru -Sua && dunst_aur
             ;;
         "main")
             display_or_not
+            ;;
+        *)
+            dunstify "Invalid option: $1"
+            #exit 1
             ;;
     esac
     kill_kitty
