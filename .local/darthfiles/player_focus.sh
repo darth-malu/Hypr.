d7c92e7526551905ac0e5d1c74db34f5a5e4a9a8
darthmalu <justinmalu@gmail.com>
2024-07-11 22:20:21 +0300

    Clean Hyprland Motions

--- .local/darthfiles/player_focus.sh
+++ .local/darthfiles/player_focus.sh
@@ -5,29 +5,30 @@ function active_player() {
 }
 
 
-grap_spotify_pid () {
+grab_spotify_pid () {
     #extracts as pid: xxxx 
     hyprctl clients | grep -i -A3 Spotify | grep pid | tr -d '[:blank:]' 
 }
 
 function focus_current_player () {
     local displaying_player=$(active_player)
-    spotify=$(grap_spotify_pid)
+    local spotify=$(grab_spotify_pid)
 
     case $displaying_player in
         *"spotify"*)
-            hyprctl dispatch focuswindow ${spotify}
+            hyprctl dispatch focuswindow ${spotify} > /dev/null
             ;;
         *"Lollypop"*)
-            hyprctl dispatch focuswindow lollypop
+            hyprctl dispatch focuswindow lollypop > /dev/null
             ;;
         *"firefox"*)
-            hyprctl dispatch focuswindow firefox
+            hyprctl dispatch focuswindow firefox > /dev/null
             ;;
         *"mpd"*)
             #hyprctl dispatch focuswindow title:nc 2>&1 /dev/null | hyprctl dispatch focuswindow title:ncmpcpp 2> /dev/null
             #hyprctl dispatch focuswindow title:nc || hyprctl dispatch focuswindow title:ncmpcpp || hyprctl dispatch exec '[workspace 6] kitty -e ncmpcpp'
-            hyprctl dispatch exec '[workspace 6] kitty -e ncmpcpp'
+            #hyprctl dispatch exec '[workspace special:nc] kitty -e ncmpcpp'
+            hyprctl dispatch togglespecialworkspace nc > /dev/null
             ;;
     esac
 }
