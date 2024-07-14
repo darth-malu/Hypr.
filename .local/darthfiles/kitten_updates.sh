985f03d35784c10af2ebb4e5f3df55397ccf3b5d
darthmalu <justinmalu@gmail.com>
2024-07-14 09:44:16 +0300

    Made updates sublime

--- .local/darthfiles/kitten_updates.sh
+++ .local/darthfiles/kitten_updates.sh
@@ -16,23 +16,30 @@ kill_kitty () {
     exit 0
 }
 
-display_updates_only () 
+updates_getter () 
 {
     #nocolor very important
     #prints out ansi color codes -yuck
-    updates="$(checkupdates --nocolor | cut -d ' ' -f1)"
-    for update in ${updates};do
-        printf "\t%s\n" "${update}"
-    done
+    #raw_updates="$(checkupdates --nocolor --nosync)"
+    #echo "$raw_updates" | cut -d ' ' -f1
+    checkupdates --nocolor --nosync | cut -d ' ' -f1
 }
 
-display_updates_only 2> /dev/null
-updates="$(display_updates_only)"
+display_updates ()
+{
+    local updates=$(updates_getter)
+
+    while read -r update;do
+        printf "\t%s\n" "${update}"
+        #echo "$update"
+    done <<< "$updates"
+}
 
 less_pkgs () 
 {
-    local temp_file="$(mktemp /media/linux_hdd/update_history_script/temp_pac_qi_array/pac_updates.XXXXXX.txt)"
+    local updates=$(updates_getter)
 
+    local temp_file="$(mktemp /media/linux_hdd/update_history_script/temp_pac_qi_array/pac_updates.XXXXXX.txt)"
     # Check if the temporary file was created successfully
     if [ ! -e "$temp_file" ]; then
         echo "Failed to create temporary file"
@@ -52,8 +59,12 @@ less_pkgs ()
 
 sed_pkgs () 
 {
+    local updates=$(updates_getter)
+
     while read -r update; do
-        pacman -Qi "$update" | sed -n -e "1p" -e "3p" -e "8p" -e "11p" -e "12p" | nl  
+        printf "%s\n" "- $update" 
+        #pacman -Qi "$update" | sed -n -e "3p" -e "8p" -e "11p" -e "12p" | nl  
+        pacman -Qi "$update" | sed -n -e "3p" -e "8p" -e "11p" -e "12p"| sed 's/^/\t/'   
         printf "\n"
     done <<< "$updates"
 }
@@ -63,10 +74,11 @@ display_or_not () {
     #set -x
     #is single line delimeted by spaces eg. pkg1 pkg2 pkg3
     #local all_updates=$(display_updates_only)
+    local updates=$(display_updates)
 
     printf "\t\t\tPackages to be updated are:\n"
     printf "\t\t\t...........................\n"
-    echo "${updates}"
+    display_updates
     echo
 
     while true;do
@@ -74,7 +86,7 @@ display_or_not () {
         printf "\t\t\t...................\n"
 
         #quiet = 1(default), verbose = 1,  minimal =2, halt =3, verbose = 1
-        printf "\n\tquiet\t\t~>\t*Default\n\tverbose\t\t  \t 1\n\tminimal \t\t 2\n\tCLEAR\t\t  \t x\n\tHALT\t\t  \t 0\n\n"
+        printf "\n\tquiet\t\t~>\t*Default\n\tverbose\t\t  \t 1\n\tminimal \t\t 2\n\tCLEAR\t\t  \t x\n\tHALT\t\t  \t 0 / q\n\n"
 
         local choice
         read -p "       \`=> " choice
@@ -90,17 +102,17 @@ display_or_not () {
 
             #Minimal
             2)
-                printf "\n ~> minimalist mode\n"
-                printf "\t\t** sed magic is working...\n\n"
-                sleep 1;sed_pkgs 
+                printf "\n=> minimalist mode\n"
+                printf "\t\t\t** sed magic is working...\n\n"
+                sed_pkgs 
                 ;;
             "x")
-                printf "\n\t Clearing Screen... Usafi muhimu\n"
+                printf "\n\t=> Clearing Screen... Usafi muhimu\n"
                 clear
                 ;;
 
             #HALT
-            0)
+            0|"q")
                 dunstify "QUITING"
                 kill_kitty
                 #exit 0
